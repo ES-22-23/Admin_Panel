902c8733cb147d243e48255ac9d3fca9597ad5d8
Danielar0w0 <danielar0000@gmail.com>
2022-12-03 17:53:14 +0000

    Implemented Intrusion Video Player

--- src/components/Intrusions/Intrusions.test.js
+++ src/components/Intrusions/Intrusions.test.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Intrusions from './Intrusions';
 
 describe('<Intrusions />', () => {
-  test('it should mount', () => {
-    render(<Intrusions />);
-    
-    const intrusions = screen.getByTestId('Intrusions');
+    test('it should mount', () => {
+        render(<Intrusions/>);
 
-    expect(intrusions).toBeInTheDocument();
-  });
+        const intrusions = screen.getByTestId('Intrusions');
+
+        expect(intrusions).toBeInTheDocument();
+    });
+
+    test('it should have a search bar', () => {
+        render(<Intrusions/>);
+
+        const searchBar = screen.getByTestId('SearchBar');
+
+        expect(searchBar).toBeInTheDocument();
+    });
 });
--- src/components/VideoModal/VideoModal.js
+++ src/components/VideoModal/VideoModal.js
@@ -1,19 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import './VideoModal.css';
-import {Modal} from "react-bootstrap";
-import { DefaultPlayer as Video } from 'react-html5video';
-import 'react-html5video/dist/styles.css';
+import {Modal, Spinner} from "react-bootstrap";
 import {FaRegEye} from "react-icons/fa";
+import {getVideoFile} from "../../utils/ApiHandler";
+import {toast} from "react-toastify";
+import Row from "react-bootstrap/Row";
+
+import { DefaultPlayer as Video } from 'react-html5video';
+import './html5video_styles.css';
 
 const VideoModal = (props) => {
 
     const [show, setShow] = useState(true);
     const [intrusion, setIntrusion] = useState(props.intrusion);
+    const [src, setSrc] = useState(undefined);
 
     useEffect(() => {
         setIntrusion(props.intrusion);
     }, [props.intrusion]);
+
+    useEffect(() => {
+        getVideoFile(intrusion.key).then(response => {
+
+            const contentType = response.headers['Content-type'];
+
+            // Binary Large Object (BLOB) is a collection of binary data stored as a single entity.
+            const blob = new Blob([response.data], {type: contentType});
+
+            setSrc(window.URL.createObjectURL(blob));
+
+        }).catch(error => {
+            console.log(error);
+            toast.error("Error loading video.");
+        });
+    }, [intrusion]);
 
     const handleClose = () => {
         setShow(false);
@@ -25,19 +46,25 @@ const VideoModal = (props) => {
                show={show} onHide={handleClose}
                style={{backgroundColor: "rgba(0,0,0,0.60)"}}
                data-testid="VideoModal">
-            <Modal.Header closeButton >
+            <Modal.Header closeButton>
                 <Modal.Title>
-                    <h5 className="align-items-center d-flex"><FaRegEye className="me-3" /> {intrusion.intrusionDate}</h5>
+                    <h5 className="align-items-center d-flex mt-2"><FaRegEye className="me-3" /> {intrusion.intrusionDate}</h5>
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h6 className="mt-2">Property ID: {intrusion.propertyID}</h6>
-                <h6 className="mb-4">Camera ID: {intrusion.cameraID}</h6>
-                <Video autoPlay loop muted
-                       controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
-                       poster="http://sourceposter.jpg">
-                    <source src="http://sourcefile.webm" type="video/webm" />
-                </Video>
+                <h6 className="mt-1 mx-2"><span style={{fontWeight: "bold"}}>Property ID:</span> {intrusion.propertyID}</h6>
+                <h6 className="mb-4 mx-2"><span style={{fontWeight: "bold"}}>Camera ID:</span> {intrusion.cameraID}</h6>
+                {src &&
+                    <Video autoPlay loop muted
+                    controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']} >
+                    <source src={src} type="video/mp4" />
+                    </Video>
+                }
+                {!src &&
+                    <Row className="d-flex justify-content-center my-5" data-testid="Loading">
+                        <Spinner animation="border" role="status"/>
+                    </Row>
+                }
             </Modal.Body>
         </Modal>
     );
