81c593f200ae62e4065f14b6c7f21a37246b8670
Danielar0w0 <danielar0000@gmail.com>
2022-12-09 23:35:38 +0000

    Added documentation

--- src/components/Notifications/Notifications.test.js
+++ src/components/Notifications/Notifications.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Notifications from './Notifications';
 
 describe('<Notifications />', () => {
-  test('it should mount', () => {
-    render(<Notifications />);
-    
-    const notifications = screen.getByTestId('Notifications');
+    test('it should mount', () => {
+        render(<Notifications/>);
 
-    expect(notifications).toBeInTheDocument();
-  });
+        const notifications = screen.getByTestId('Notifications');
+        expect(notifications).toBeInTheDocument();
+    });
 });
